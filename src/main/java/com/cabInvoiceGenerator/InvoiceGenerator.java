d2b159701e25ff68037a1fbd171ef59b3d44b356
sheetal-10 <sheetalp257@gmail.com>
2021-09-27 00:05:26 +0530

    Added: code to get get minimum fare for less distance and time

--- src/main/java/com/cabInvoiceGenerator/InvoiceGenerator.java
+++ src/main/java/com/cabInvoiceGenerator/InvoiceGenerator.java
@@ -3,9 +3,12 @@ package com.cabInvoiceGenerator;
 public class InvoiceGenerator {
     private static final double MINIMUM_COST_PER_KM = 10;
     private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5.0;
 
     public double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
-
 }
