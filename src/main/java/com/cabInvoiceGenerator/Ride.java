a77cbbc063a4f7c5d6651b82496ae54e6498880d
sheetal-10 <sheetalp257@gmail.com>
2021-09-28 00:20:17 +0530

    Refactored: test case and code using enum supporting premium and normal rides

--- src/main/java/com/cabInvoiceGenerator/Ride.java
+++ src/main/java/com/cabInvoiceGenerator/Ride.java
@@ -3,9 +3,13 @@ package com.cabInvoiceGenerator;
 public class Ride {
     public final double distance;
     public final int time;
+    public final CabRide cabRide;
 
-    public Ride(double distance, int time) {
+
+    public Ride(CabRide cabRide, double distance, int time) {
+        this.cabRide = cabRide;
         this.distance = distance;
         this.time = time;
+
     }
 }
--- src/test/java/com/cabInvoiceGenerator/InvoiceServiceTest.java
+++ src/test/java/com/cabInvoiceGenerator/InvoiceServiceTest.java
@@ -5,11 +5,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
+    Ride[] rides = null;
     InvoiceService invoiceService = null;
+    InvoiceSummary expectedInvoiceSummary = null;
+    private RideRepository rideRepository = null;
 
     @BeforeEach
     public void setUp() throws Exception {
         invoiceService = new InvoiceService();
+        rideRepository = new RideRepository();
+        invoiceService.setRideRepository(rideRepository);
+        rides = new Ride []{
+                new Ride(CabRide.NORMAL, 2.0, 5),
+        new Ride(CabRide.PREMIUM, 0.1,1)
+                };
+        expectedInvoiceSummary = new InvoiceSummary(2,45.0);
     }
 
     @Test
@@ -30,24 +40,15 @@ public class InvoiceServiceTest {
 
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-        Ride[] rides = {new Ride(2.0, 5),
-                new Ride(0.1, 1)
-        };
-        InvoiceSummary actualSummary = invoiceService.calculateFare(rides);
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
-        Assertions.assertEquals(expectedInvoiceSummary, actualSummary);
+        InvoiceSummary summary = invoiceService.calculateFare(rides);
+        Assertions.assertEquals(expectedInvoiceSummary, summary);
     }
 
     @Test
     public void givenUserIdAndRides_ShouldReturnInvoiceSummary() {
         String userId = "a@b.com";
-        Ride[] rides = {new Ride(2.0, 5),
-                new Ride(0.1, 1)
-        };
         invoiceService.addRides(userId, rides);
         InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
         Assertions.assertEquals(expectedInvoiceSummary, summary);
     }
-
 }
